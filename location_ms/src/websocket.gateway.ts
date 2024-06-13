9fcf79a82c584d693c2341c00de7c551c6bdb201
CCChira <chira.cristian621@gmail.com>
2024-06-13 17:20:56 +0300

    changes to location ms

--- location_ms/src/websocket.gateway.ts
+++ location_ms/src/websocket.gateway.ts
@@ -4,7 +4,7 @@ import {
   SubscribeMessage,
   OnGatewayConnection,
   OnGatewayDisconnect,
-  ConnectedSocket, MessageBody
+  ConnectedSocket, MessageBody, OnGatewayInit
 } from '@nestjs/websockets';
 import {Server, Socket} from 'socket.io';
 
@@ -13,12 +13,23 @@ import {Server, Socket} from 'socket.io';
     origin: '*',
   }
 })
-export class LocationGateway implements OnGatewayConnection, OnGatewayDisconnect {
+export class LocationGateway implements OnGatewayConnection, OnGatewayDisconnect, OnGatewayInit {
   @WebSocketServer()
   server: Server;
 
   private userSocket: Socket | null = null; // Socket for the single user
 
+  afterInit(server: Server) {
+    console.log('WebSocket Gateway initialized');
+  }
+
+  handleConnection(client: Socket) {
+    console.log(`Client connected: ${client.id}`);
+  }
+
+  handleDisconnect(client: Socket) {
+    console.log(`Client disconnected: ${client.id}`);
+  }
   @SubscribeMessage('register_user')
   handleRegisterUser(@ConnectedSocket() client: Socket) {
     // Assuming the single user sends a specific message to register
@@ -28,6 +39,7 @@ export class LocationGateway implements OnGatewayConnection, OnGatewayDisconnect
 
   @SubscribeMessage('send_location')
   handleLocationUpdate(@MessageBody() data: { deviceId: string, latitude: number, longitude: number }) {
+    console.log(data);
     console.log(`Received location from ${data.deviceId}: ${data.latitude}, ${data.longitude}`);
     // Send data to the single user, if registered
     if (this.userSocket) {
